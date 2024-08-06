514a0d6a225a9727ed427c09630e4564fba8f10a
lewistorrington <lewis.torrington@upcloud.com>
2024-08-06 18:12:11 +0300

    feat: some sort of TDD

---
+++ example_python_files/1.py
@@ -0,0 +1,13 @@
+print("Test that we can do operations on basic types like ints, strings, floats, etc.")
+print("-------------")
+
+
+my_int_result = 1 + 4084
+my_string_result = "Hello " + "world"
+my_float_result = 3.1389 + 83.21
+
+
+print("Int operation result:", my_int_result)
+print("Str operation result:", my_string_result)
+print("Float operation result:", my_float_result)
+
